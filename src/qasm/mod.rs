f06c335298f3b4f9de05e372c23cfa97f8858d24
MucTepDaH16 <denisdrozhzhin1999@gmail.com>
2022-09-07 16:04:02 +0300

    test-case crate

--- src/qasm/mod.rs
+++ src/qasm/mod.rs
@@ -7,117 +7,34 @@ pub use int::Int;
 pub use sym::Sym;
 
 #[cfg(test)]
-mod qasm_sources {
+mod tests {
+    use test_case::test_case;
+
     use super::*;
 
-    fn run_qasm<'s>(source: &'s str) {
-        let ast = Ast::<'s>::from_source(source).unwrap();
+    #[test_case(include_str!("./examples/source/adder.qasm"); "adder")]
+    #[test_case(include_str!("./examples/source/bigadder.qasm"); "big_adder")]
+    #[test_case(include_str!("./examples/source/Deutsch_Algorithm.qasm"); "Deutsch_Algorithm")]
+    #[test_case(include_str!("./examples/source/inverseqft1.qasm"); "inverse_qft_1")]
+    #[test_case(include_str!("./examples/source/inverseqft2.qasm"); "inverse_qft_2")]
+    #[test_case(include_str!("./examples/source/ipea_3_pi_8.qasm"); "ipea_3_pi_8")]
+    #[test_case(include_str!("./examples/source/qe_qft_3.qasm"); "qe_qft_3")]
+    #[test_case(include_str!("./examples/source/qe_qft_4.qasm"); "qe_qft_4")]
+    #[test_case(include_str!("./examples/source/qe_qft_5.qasm"); "qe_qft_5")]
+    #[test_case(include_str!("./examples/source/qec.qasm"); "qec")]
+    #[test_case(include_str!("./examples/source/qft.qasm"); "qft")]
+    #[test_case(include_str!("./examples/source/qpt.qasm"); "qpt")]
+    #[test_case(include_str!("./examples/source/rb.qasm"); "rb")]
+    #[test_case(include_str!("./examples/source/teleport.qasm"); "teleport")]
+    #[test_case(include_str!("./examples/source/teleportv2.qasm"); "teleport_v2")]
+    #[test_case(include_str!("./examples/source/W-state.qasm"); "w_state")]
+    #[test_case(include_str!("./examples/source/W3test.qasm"); "W3test")]
+    fn run_qasm(source: &'static str) {
+        let ast = Ast::from_source(source).unwrap();
         let int = Int::new(ast).unwrap();
         let mut sym = Sym::new(int);
 
         sym.reset();
         sym.finish();
-    }
-
-    #[test]
-    fn adder() {
-        let source = include_str!("./examples/source/adder.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn bigadder() {
-        let source = include_str!("./examples/source/bigadder.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn deutsch_algorithm() {
-        let source = include_str!("./examples/source/Deutsch_Algorithm.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn inverse_qft_1() {
-        let source = include_str!("./examples/source/inverseqft1.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn inverse_qft_2() {
-        let source = include_str!("./examples/source/inverseqft2.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn ipea_3_pi_8() {
-        let source = include_str!("./examples/source/ipea_3_pi_8.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn qe_qft_3() {
-        let source = include_str!("./examples/source/qe_qft_3.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn qe_qft_4() {
-        let source = include_str!("./examples/source/qe_qft_4.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn qe_qft_5() {
-        let source = include_str!("./examples/source/qe_qft_5.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn qec() {
-        let source = include_str!("./examples/source/qec.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn qft() {
-        let source = include_str!("./examples/source/qft.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn qpt() {
-        let source = include_str!("./examples/source/qpt.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn rb() {
-        let source = include_str!("./examples/source/rb.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn teleport() {
-        let source = include_str!("./examples/source/teleport.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn teleport_v2() {
-        let source = include_str!("./examples/source/teleportv2.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn w_state() {
-        let source = include_str!("./examples/source/W-state.qasm");
-        run_qasm(source);
-    }
-
-    #[test]
-    fn w3_test() {
-        let source = include_str!("./examples/source/W3test.qasm");
-        run_qasm(source);
     }
 }
