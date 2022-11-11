1e0e2843a11dae07a32ce0c5f69f2f53e5063316
MucTepDaH16 <denisdrozhzhin1999@gmail.com>
2022-11-11 19:00:41 +0300

    Fix bits iter
    Signed-off-by: MucTepDaH16 <denisdrozhzhin1999@gmail.com>

--- src/math/bits_iter.rs
+++ src/math/bits_iter.rs
@@ -15,11 +15,33 @@ impl Iterator for BitsIter {
     fn next(&mut self) -> Option<Self::Item> {
         loop {
             if self.pos & self.bits != 0 {
-                return Some(self.pos);
+                let pos = self.pos;
+                self.pos <<= 1;
+                return Some(pos);
             } else if self.pos > self.bits {
                 return None;
             }
             self.pos <<= 1;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bits_iter() {
+        let number = 0b10011001101010;
+        let mut iter = BitsIter::from(number);
+
+        assert_eq!(iter.next(), Some(1 << 1));
+        assert_eq!(iter.next(), Some(1 << 3));
+        assert_eq!(iter.next(), Some(1 << 5));
+        assert_eq!(iter.next(), Some(1 << 6));
+        assert_eq!(iter.next(), Some(1 << 9));
+        assert_eq!(iter.next(), Some(1 << 10));
+        assert_eq!(iter.next(), Some(1 << 13));
+        assert_eq!(iter.next(), None);
     }
 }
